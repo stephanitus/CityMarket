8f3a86da7ac9778f4266669c0242e010840ebe6a
Stephanitus <stephans@asia.com>
2020-02-16 11:03:58 -0500

    Minimerge

--- src/BusinessPage.js
+++ src/BusinessPage.js
@@ -6,19 +6,13 @@ class BusinessPage extends Component{
     constructor(props){
         super(props);
         this.state = {
-<<<<<<< HEAD
           items: this.props.items,
           name: this.props.name,
           description: this.props.description
-=======
-          items: this.props.item,
-          name: this.props.name,
-          description: this.props/description
->>>>>>> f94196ebb62916cdcc019a528644601015cf8c4a
         };
       }
 
-      
+
     render() {
         return (
             <div className="wrapper">
